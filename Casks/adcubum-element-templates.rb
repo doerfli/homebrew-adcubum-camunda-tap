f28f93f6bbb8352dbe76b39557c9e98e9c9ec368
Marc Doerflinger <marc.doerflinger@adcubum.com>
2021-05-17 17:13:33 +0200

    add cask

--- Casks/adcubum-element-templates.rb
+++ Casks/adcubum-element-templates.rb
@@ -1,7 +1,7 @@
-cask "modeler-element-templates" do
+cask "adcubum-element-templates" do
     version "0.0.1"
-    sha256 "5ffe028fde649858e8dc09e394d0910f601737ffa559eef3ae3494c1893ff27f"
-    url "https://github.com/doerfli/adcubum-camunda/raw/main/Zips/modeler-element-templates-0.0.1.zip"
+    sha256 "8695b8876c5ab7afbf3ea9f098a0847c26696dee9937d1313dab575ececf4c77"
+    url "https://github.com/doerfli/adcubum-camunda/raw/main/Zips/adcubum-element-templates-0.0.1.zip"
     name "Camunda Modeler Adcubum element templates"
     desc "Adcubum specific element templates for the Camunda Modeler"
     
