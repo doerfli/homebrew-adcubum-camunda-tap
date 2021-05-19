7bac167e4409a310556f1099f64e8a6c7897faf4
Marc Doerflinger <marc.doerflinger@adcubum.com>
2021-05-19 13:56:31 +0200

    formatting

--- Casks/adcubum-element-templates.rb
+++ Casks/adcubum-element-templates.rb
@@ -1,19 +1,19 @@
 cask "adcubum-element-templates" do
-    version "1.0.1"
-    sha256 "77222bfe4306648f0f7c0d035342e1b6634fab9a0477204ba3a37e30135f579f"
-    url "https://github.com/doerfli/adcubum-camunda/raw/main/Zips/camunda-modeler-element-templates-1.0.1@a2afb0aff9b.zip"
-    name "Camunda Modeler Adcubum element templates"
-    desc "Adcubum specific element templates for the Camunda Modeler"
-    
-    installer script: {
-      executable:   "#{staged_path}/install.sh",
-    #   args:         ["-q"],
-    #   sudo:         true,
-    #   print_stderr: false,
-    }
+  version "1.0.1"
+  sha256 "77222bfe4306648f0f7c0d035342e1b6634fab9a0477204ba3a37e30135f579f"
+  url "https://github.com/doerfli/adcubum-camunda/raw/main/Zips/camunda-modeler-element-templates-1.0.1@a2afb0aff9b.zip"
+  name "Camunda Modeler Adcubum element templates"
+  desc "Adcubum specific element templates for the Camunda Modeler"
+  
+  installer script: {
+    executable:   "#{staged_path}/install.sh",
+  #   args:         ["-q"],
+  #   sudo:         true,
+  #   print_stderr: false,
+  }
 
-    zap trash: [
-       "~/Library/Application\ Support/camunda-modeler/resources/element-templates"
-    ]
-  end
+  zap trash: [
+    "~/Library/Application\ Support/camunda-modeler/resources/element-templates",
+  ]
+end
   
