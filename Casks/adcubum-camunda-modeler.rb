e93d836f65ff056f6a8b24e752fade7a3b18b4ed
Marc Doerflinger <marc.doerflinger@adcubum.com>
2021-06-03 15:19:12 +0200

    add checksum for adcubum-camunda-modeler

--- Casks/adcubum-camunda-modeler.rb
+++ Casks/adcubum-camunda-modeler.rb
@@ -1,6 +1,6 @@
 cask "adcubum-camunda-modeler" do
   version :latest
-  sha256 :no_check
+  sha256 "2081cb0ba9c3e4a82a84a0efb2c403a5c8691e7ec144135ed197f06719994f02"
 
   url "https://github.com/doerfli/homebrew-adcubum-camunda-tap/raw/main/Zips/hello-world.zip"
   name "Adcubum Camunda Modeler package"
